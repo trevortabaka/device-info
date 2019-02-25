da257944317ab3029e4f23afaf1e3ffe1ec4f564
tntabaka <get@trevortabaka.info>
2019-02-24 21:25:13 -0500

    Add GPLv3 license info

--- app/src/main/java/info/trevortabaka/deviceinfo/api/UnitValue.java
+++ app/src/main/java/info/trevortabaka/deviceinfo/api/UnitValue.java
@@ -1,10 +1,28 @@
+/*
+Copyright (C) 2019 Trevor Tabaka
+
+This file is part of Device Info.
+
+Device Info is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Device Info is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Device Info.  If not, see <https://www.gnu.org/licenses/>.
+*/
 package info.trevortabaka.deviceinfo.api;
 
 class UnitValue<T> implements ApiValue {
     private final T value;
     private final String unit;
 
-    public UnitValue(T value, String unit) {
+    UnitValue(T value, String unit) {
         this.value = value;
         this.unit = unit;
     }
--- app/src/test/java/info/trevortabaka/deviceinfo/ui/SortingStrategiesTest.java
+++ app/src/test/java/info/trevortabaka/deviceinfo/ui/SortingStrategiesTest.java
@@ -1,5 +1,23 @@
-package info.trevortabaka.deviceinfo.ui;
+/*
+Copyright (C) 2019 Trevor Tabaka
 
+This file is part of Device Info.
+
+Device Info is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Device Info is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Device Info.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package info.trevortabaka.deviceinfo.ui;
+l
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -63,7 +81,12 @@ public class SortingStrategiesTest {
         assertEquals("C", apis.get(2).getClassName());
     }
 
-    private static class A { }
-    private static class B { }
-    private static class C { }
+    private static class A {
+    }
+
+    private static class B {
+    }
+
+    private static class C {
+    }
 }
